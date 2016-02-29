d14fb5ed3833fff29b9a0b5d8e7c2f0e9da23d36
Jorge Capona <jcapona@gmail.com>
2016-02-28 21:54:59 -0300

    Adds CRUD routes and links to controller actions

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/books");
 
 // App configuration
 app.use(bodyParser.json());
@@ -10,9 +11,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride());
 
 app.get("/", function(req, res) { 
- res.send("Hello World!");
+  res.send("Hello World!");
 });
+
+// CRUD operations
+var BooksController = require('./controllers/BooksController');
+app.post('/book',BooksController.create); // Create
+app.get('/book/:id',BooksController.readOne); // Read One
+app.get('/book/',BooksController.readAll); // Read All
+app.put('/book/:id',BooksController.update); // Update
+app.delete('/book/:id',BooksController.delete); // Delete
 
 app.listen(5000, function(){
  console.log("index.js listening on port 5000");
-})
+});
